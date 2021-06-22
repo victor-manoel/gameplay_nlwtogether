8fd78896ab55332bc5a3a69b3a4b8e2efd4b744f
Victor Manoel <victormanoel7895123@hotmail.com>
2021-06-21 21:32:34 -0300

    Create Index

---
+++ src/screens/SingIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function SignIn() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
